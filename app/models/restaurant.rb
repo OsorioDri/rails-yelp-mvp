c82653ff04fb05bebc5942da4f0904e9a8324e16
Adriana Osorio <driosorio@gmail.com>
2022-05-25 15:17:53 -0300

    models raked

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
-  validates :name, presence: true
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: %w[chinese italian japanese french belgian], message: '%<value> is not a valid category' }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database'
+
+Restaurant.destroy_all
+
+puts 'Database Clean'
+
+puts 'Creating Restaurants'
+
+50.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  puts restaurant.name
+end
+
+puts 'Finished'
